9145cef35c161ad75d22b26daeee768c7a436af9
Kaushik varma <iamkaushikvarma@gmail.com>
2023-04-29 12:02:36 -0500

    first commit

---
+++ category/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    Category_name = models.CharField(max_length=50,unique=True)
+    #slug is nothing but urls
+    slug = models.SlugField(max_length=100,unique=True)
+    description = models.TextField(max_length=255)
+    #here the images will be uploaded to 'photos/categories'
+    cat_image = models.ImageField(upload_to ='photos/categories/',blank=True)
+ 
+    #django admin site usually add 's' to the models we create basically it pluralises
+    #to make it right we use below code
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    #string representation of model
+    def __str__(self):
+        return self.Category_name
+        
